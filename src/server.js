053df3abb85b7ca36c88eb91e9e47eadb6889153
cfragoulis-design <cfragoulis@gmail.com>
2026-05-24 11:27:40 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 
 const prisma = new PrismaClient();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
